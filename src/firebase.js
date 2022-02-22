f815a065eb743786d7803f32fa7004293fbb2af2
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-02-22 22:25:37 +0530

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5mb7AaVOMeYxLK1X5CybzTpWnYKY7j3M",
+  authDomain: "netflix-clone-668b8.firebaseapp.com",
+  projectId: "netflix-clone-668b8",
+  storageBucket: "netflix-clone-668b8.appspot.com",
+  messagingSenderId: "210398332164",
+  appId: "1:210398332164:web:af097fbb213d4ccd7abc51",
+};
